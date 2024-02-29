a870c6909d00a4189feccca213c29dd16f65a1da
Jeff Roach <37549850+ArchwayEon@users.noreply.github.com>
2024-02-29 15:32:42 -0500

    Started lab 7

--- JRInteractiveGraphicsS24/Generate.h
+++ JRInteractiveGraphicsS24/Generate.h
@@ -10,7 +10,15 @@ public:
 		float width, float height, float depth,
 		glm::vec3 color = { 1.0f, 1.0f, 1.0f },
 		glm::vec2 tex = { 1.0f, 1.0 });
+	static std::shared_ptr<VertexBuffer> CuboidWithNormals(
+		float width, float height, float depth,
+		glm::vec3 color = { 1.0f, 1.0f, 1.0f },
+		glm::vec2 tex = { 1.0f, 1.0 });
 	static std::shared_ptr<VertexBuffer> XZPlane(
+		float width, float depth,
+		glm::vec3 color = { 1.0f, 1.0f, 1.0f },
+		glm::vec2 tex = { 1.0f, 1.0 });
+	static std::shared_ptr<VertexBuffer> XZPlaneWithNormals(
 		float width, float depth,
 		glm::vec3 color = { 1.0f, 1.0f, 1.0f },
 		glm::vec2 tex = { 1.0f, 1.0 });
